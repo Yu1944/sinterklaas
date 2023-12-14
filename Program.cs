106a12837304dc73df64e2ef42ac0856088c11bf
Yu1944 <96294541+Yu1944@users.noreply.github.com>
2023-12-15 00:14:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace verhaalsinterklaas;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\rei\Desktop\verhaalsinterklaas\verhaal.txt";
+        string[] stories = File.ReadAllLines(filePath);
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(0, stories.Length);
+        
+        try
+        {
+            string name = args[0];
+            string present = args[1];
+            Console.WriteLine($"Hallo {name} jouw cadeau is {present} {stories[randomNumber]}");
+        }
+        catch (System.Exception)
+        {
+            System.Console.WriteLine("need atleast 2 arguyment");
+            throw;
+        }
+    }
+}
